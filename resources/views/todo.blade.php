30fec8800257e5917d7aa520ebbca44e61654804
StevenTanjung <stv_tanjung@yahoo.co.id>
2017-03-13 11:23:39 +0700

    first commit

---
+++ resources/views/todo.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ToDo List</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<h1>ToDo List</h1>
+	<ul>
+		<li>Taki</li>
+		<li>Mitsuha</li>
+		<li>Yotsuha</li>
+	</ul>
+	<form>
+		<div class="row">
+			<div class="col-lg-3">
+				<div class="form-group">
+					<input type="text" name="todo_item" class="form-control" placeholder="Item Baru...">
+				</div>
+			</div>
+			<div class="col-lg-1">
+				
+			</div>
+			<div class="col-lg-8">
+				
+			</div>
+		</div>
+	</form>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		</div>
+	</div>
+</div>	
+</body>
+</html>
